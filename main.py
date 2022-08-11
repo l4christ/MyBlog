4ac637f85496025bb5fdb4e6908130eb0bb08864
l4christ <ademolaemmanuel12@yahoo.com>
2022-08-11 16:22:08 +0100

    Fix SQLAlchemy 1.4.x not connecting to HerokuPostgres

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ uri = os.getenv("DATABASE_URL")  # or other relevant config var
 if uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
