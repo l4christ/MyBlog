f068f4a3b208153032841e6b0ad0e76a8c460401
l4christ <ademolaemmanuel12@yahoo.com>
2022-08-11 15:47:17 +0100

    fixing app crash error

--- main.py
+++ main.py
@@ -232,5 +232,4 @@ def delete_post(post_id):
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=5000)
